0c76a42a55a6dcfa59e5fe43c2c51d84922116d7
Venkata Siva Prasad Kakumani <112908439+SivaKakumani-kvsp@users.noreply.github.com>
2023-09-27 09:32:45 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+<div id="footer"><!-- footer begin -->
+    <div class="container"><!-- container begin -->
+        <div class="row"><!-- row begin -->
+            <div class="col-sm-6 col-md-3"><!-- col sm begin -->
+                
+                <h4>Pages</h4>
+                <ul><!-- ul begin -->
+                    <li><a href="cart.php">shopping cart</a></li>
+                    <li><a href="shop.php">pantry</a></li>
+                    <li><a href="contact.php">contact us</a></li>
+                    <li><a href="customer/my_account.php">my Account</a></li>
+                </ul><!-- ul finish -->
+                <hr>
+                <h4>User Section</h4>
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="../customer_register.php">Register</a></li>
+                </ul>
+                <hr class="hidden-md hidden-lg hidden-sm">
+
+            </div><!-- col sm finish -->
+            <div class="col-sm-6 col-md-3"><!-- col sm-6col-md-3 begin -->
+                <h4>Most Loved Products</h4>
+                <ul><!-- ul begin -->
+                    <li><a href="#">Milk</a></li>
+                    <li><a href="#">Eggs</a></li>
+                    <li><a href="#">Cheese</a></li>
+                    <li><a href="#">Cereals</a></li>
+                </ul><!-- ul finish -->
+                <hr class="hidden-md hidden-lg">
+            </div><!-- col sm-6col-md-3 finish -->
+
+            <div class="col-sm-6 col-md-3"><!-- col sm-6col-md-3 begin -->
+                <h4>Find Us:</h4>
+                <p><!-- p Start -->
+                    <strong>Paws Pantry</strong>
+                    <br/>room no-
+                    <br/>Campus center,IUPUI
+                    <br/>0818-0683-3157
+                    <br/>mugianto4th@gmail.com
+                    <br/><strong>Admin</strong>
+                </p><!-- p Finish -->
+                <a href="contact.php">Check Our Contact Page</a>
+                <hr class="hidden-md hidden-lg">
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            <div class="col-sm-6 col-md-3">
+                <h4>Get The News</h4>
+                <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                </p>
+                <form action="" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        <input type="text" class="form-control" name="email">
+                        <span class="input-group-btn"> <!-- input-group-btn begin -->
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                        </span><!-- input-group-btn Finish -->
+                    </div>
+                </form>
+                <hr>
+                <h4>Keep In Touch</h4>
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>      
+            </div><!-- col sm-6col-md-3 finish -->
+        </div><!-- row finish -->
+    </div><!-- container Finish -->
+</div><!-- footer finish -->
